f845fe5012a540f6b28e2af11c91c04500ad43da
Marcelo Moura <mgmoura@gmail.com>
2022-10-26 10:41:09 -0300

    projeto criado

---
+++ src/main/java/com/mgmoura/bookstoreweb/BookstorewebApplication.java
@@ -0,0 +1,13 @@
+package com.mgmoura.bookstoreweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstorewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstorewebApplication.class, args);
+	}
+
+}
