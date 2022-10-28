404e72194dd819785ca30c59c78645554708b229
Marcelo Moura <mgmoura@gmail.com>
2022-10-28 12:21:38 -0300

    instancia automatica

--- src/main/java/com/mgmoura/bookstoreweb/BookstorewebApplication.java
+++ src/main/java/com/mgmoura/bookstoreweb/BookstorewebApplication.java
@@ -1,13 +1,42 @@
 package com.mgmoura.bookstoreweb;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mgmoura.bookstoreweb.domain.Categoria;
+import com.mgmoura.bookstoreweb.domain.Livro;
+import com.mgmoura.bookstoreweb.repositories.CategoriaRepository;
+import com.mgmoura.bookstoreweb.repositories.LivroRepository;
+
 @SpringBootApplication
-public class BookstorewebApplication {
+public class BookstorewebApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;	
+	@Autowired
+	private LivroRepository livroRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstorewebApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática", "descrição livros");
+		
+		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "lorem ipsum", cat1);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+		
+		
 	}
 
 }
